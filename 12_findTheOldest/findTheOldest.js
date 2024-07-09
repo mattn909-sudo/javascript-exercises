fb702b0034ed6d74cb2be1f167213cc24756aa12
Matt Mac <mattn9091@gmail.com>
2024-07-09 13:53:18 -0400

    Sort people array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(person) {
+    const date = new Date();
+    const oldest = person.sort((a,b) => {
+        let lastGuy = 0;
+        let nextGuy = 0;
+        "yearOfDeath" in a ? lastGuy = a.yearOfDeath - a.yearOfBirth : lastGuy = date.getFullYear() - a.yearOfBirth;
+        "yearOfDeath" in b ? nextGuy = b.yearOfDeath - b.yearOfBirth : nextGuy = date.getFullYear() - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+    return oldest[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
